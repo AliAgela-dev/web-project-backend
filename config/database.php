a3d7f071e307c5bda7d2be04a631e0ac44db5f0d
Ali-agela <aliagela09@gmail.com>
2025-07-01 17:08:21 +0200

    added docker

--- config/database.php
+++ config/database.php
@@ -9,11 +9,11 @@
  */
 
 return [
-    'host' => '127.0.0.1', // or 'localhost'
+    'host' => 'postgresql://ali:P42L1AGw5lAPIsJI3ryuk7r2uMSChrST@dpg-d1hv2ejuibrs7380k6gg-a.oregon-postgres.render.com', 
     'port' => 5432,
-    'dbname' => 'web_project', // The name of your database
-    'user' => 'ali',      // Your database username
-    'password' => getenv('DB_PASSWORD'), // Get database password from environment variable
+    'dbname' => 'web_project_ll18', 
+    'user' => 'ali',      
+    'password' => getenv('DB_PASSWORD'), 
     'charset' => 'utf8mb4'
 ];
 
